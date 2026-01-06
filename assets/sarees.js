21820ba4c8649ad18cb7b4cd22650c7f7415fd0c
i101 <it01@himak.in>
2026-01-06 12:54:50 +0530

    first commit

---
+++ assets/sarees.js
@@ -0,0 +1,43 @@
+const sareeData = {
+  silk: [
+    {
+      name: "Royal Silk Saree",
+      price: "₹5,999",
+      image: "assets/img/silk.png"
+    },
+    {
+      name: "Kanchipuram Silk",
+      price: "₹7,499",
+      image: "assets/img/silk.png"
+    }
+  ],
+
+  cotton: [
+    {
+      name: "Soft Cotton Saree",
+      price: "₹2,499",
+      image: "assets/img/cotton.png"
+    },
+    {
+      name: "Printed Cotton Saree",
+      price: "₹2,999",
+      image: "assets/img/cotton.png"
+    }
+  ],
+
+  banarasi: [
+    {
+      name: "Banarasi Zari Saree",
+      price: "₹8,999",
+      image: "assets/img/banarasi.png"
+    }
+  ],
+
+  bridal: [
+    {
+      name: "Bridal Designer Saree",
+      price: "₹14,999",
+      image: "assets/img/bridal.png"
+    }
+  ]
+};
